00b3cdbafe3fdc9f6737f7e9aa041b152667533b
rissol86 <119512061+rissol86@users.noreply.github.com>
2023-06-28 18:14:42 +0300

    123

---
+++ app.js
@@ -0,0 +1,12 @@
+let byrger = document.querySelector(".byrger")
+
+let joe = document.querySelector(".ul-mobile")
+
+function A(){
+        if(joe.style.display=="none"){
+                joe.style.display="block"
+              }else{
+                joe.style.display="none"
+              }
+}
+
